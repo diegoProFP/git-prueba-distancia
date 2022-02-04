506cdb357af3f6ffdc6314a77d22b2b95e7707b1
debian <diego.di1@educa.madrid.org>
2022-02-04 21:16:47 +0100

    1 Cambio - consola

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo!");
+		System.out.println("Hola mundo! Cambio desde GitRepo");
 	}
 
 }
